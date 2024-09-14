75ebb910c67fa4086cdb191fe2b0260a4ed2725c
Hamdan Patel <hpatel164@hawk.iit.edu>
2024-09-13 20:57:54 -0500

    Added new sidebar

--- src/pages/sidebar.js
+++ src/pages/sidebar.js
@@ -77,77 +77,22 @@ const Sidebar = () => {
           <h4>Doorlocks</h4>
           <ul>
             <li>
-              <a href="/doorlocks?maker=Yale">Yale</a>
+              <a href="/doorlock?maker=Yale">Yale</a>
             </li>
             <li>
-              <a href="/doorlocks?maker=Kwikset">Kwikset</a>
+              <a href="/doorlock?maker=Kwikset">Kwikset</a>
             </li>
             <li>
-              <a href="/doorlocks?maker=Schlage">Schlage</a>
+              <a href="/doorlock?maker=Schlage">Schlage</a>
             </li>
             <li>
-              <a href="/doorlocks?maker=SwitchBot">Switch Bot</a>
+              <a href="/doorlock?maker=SwitchBot">Switch Bot</a>
             </li>
             <li>
-              <a href="/doorlocks?maker=Ultraloq">Ultraloq</a>
+              <a href="/doorlock?maker=Ultraloq">Ultraloq</a>
             </li>
             <li>
-              <a href="/doorlocks?maker=Wyze">Wyze</a>
-            </li>
-          </ul>
-        </li>
-
-        <li>
-          <h4>About us</h4>
-          <ul>
-            <li class="text">
-              <p style={{ margin: 0 }}>
-                Aenean nec massa a tortor auctor sodales sed a dolor. Duis vitae
-                lorem sem. Proin at velit vel arcu pretium luctus.{" "}
-                <a href="#" class="readmore">
-                  Read More &raquo;
-                </a>
-              </p>
-            </li>
-          </ul>
-        </li>
-
-        <li>
-          <h4>Search site</h4>
-          <ul>
-            <li class="text">
-              <form method="get" class="searchform" action="#">
-                <p>
-                  <input type="text" size="31" value="" name="s" class="s" />
-                </p>
-              </form>
-            </li>
-          </ul>
-        </li>
-
-        <li>
-          <h4>Helpful Links</h4>
-          <ul>
-            <li>
-              <a
-                href="http://www.themeforest.net/?ref=spykawg"
-                title="premium templates"
-              >
-                Premium HTML web templates from $10
-              </a>
-            </li>
-            <li>
-              <a
-                href="http://www.dreamhost.com/r.cgi?259541"
-                title="web hosting"
-              >
-                Cheap web hosting from Dreamhost
-              </a>
-            </li>
-            <li>
-              <a href="http://tuxthemes.com" title="Tux Themes">
-                Premium WordPress themes
-              </a>
+              <a href="/doorlock?maker=Wyze">Wyze</a>
             </li>
           </ul>
         </li>
